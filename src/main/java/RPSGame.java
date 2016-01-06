592a935f9e6d075f8c2bcdf1ec237c77c24442e2
Illia Kuchko and Nathan Romike <student@epicodus.com>
2016-01-06 14:57:23 -0800

    Fixed .equals issues with user input method running on spark.

--- src/main/java/RPSGame.java
+++ src/main/java/RPSGame.java
@@ -5,20 +5,42 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class RPSGame {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
+
+      String playerOneChoice = request.queryParams("PlayerOne");
+      String playerTwoChoice = request.queryParams("PlayerTwo");
+
+      boolean gameResult = checkWinner(playerOneChoice, playerTwoChoice);
+
+      model.put("res", gameResult);
+      return new ModelAndView(model, layout);
+
+      }, new VelocityTemplateEngine());
+  }
 
   public static Boolean checkWinner(String firstPlayerChoice, String secondPlayerChoice) {
     if ((firstPlayerChoice == "Rock") && (secondPlayerChoice == "Scissors")) {
       return true;
-    } else if ((firstPlayerChoice == "Scissors") && (secondPlayerChoice == "Rock")) {
+    } else if ((firstPlayerChoice.equals("Scissors")) && (secondPlayerChoice.equals("Rock"))) {
       return false;
-    } else if ((firstPlayerChoice == "Rock") && (secondPlayerChoice == "Paper")) {
+    } else if ((firstPlayerChoice.equals("Rock")) && (secondPlayerChoice.equals("Paper"))) {
       return false;
-    } else if ((firstPlayerChoice =="Paper") && (secondPlayerChoice == "Rock")) {
+    } else if ((firstPlayerChoice.equals("Paper")) && (secondPlayerChoice.equals("Rock"))) {
       return true;
-    } else if ((firstPlayerChoice == "Scissors") && (secondPlayerChoice == "Paper")) {
+    } else if ((firstPlayerChoice.equals("Scissors")) && (secondPlayerChoice.equals("Paper"))) {
       return true;
-    } else if ((firstPlayerChoice == "Paper") && (secondPlayerChoice == "Scissors")) {
+    } else if ((firstPlayerChoice.equals("Paper")) && (secondPlayerChoice.equals("Scissors"))) {
       return false;
     }else {
       return null;
