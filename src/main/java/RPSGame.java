9c67e7d896598454cdccf2f859e55f595dc0b10f
Illia Kuchko and Nathan Romike <student@epicodus.com>
2016-01-06 12:01:17 -0800

    passed Scissors beat Rock test.

--- src/main/java/RPSGame.java
+++ src/main/java/RPSGame.java
@@ -3,3 +3,17 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+
+public class RPSGame {
+  public static void main(String[] args) {}
+
+  public static Boolean checkWinner(String firstPlayerChoice, String secondPlayerChoice) {
+    if ((firstPlayerChoice == "Rock") && (secondPlayerChoice == "Scissors")) {
+      return true;
+    } else if ((firstPlayerChoice == "Scissors") && (secondPlayerChoice == "Rock")) {
+      return false;
+    } else {
+      return null;
+    }
+  }
+}
--- src/test/java/RPSGameTest.java
+++ src/test/java/RPSGameTest.java
@@ -1,2 +1,25 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+
+public class RPSGameTest {
+
+  @Test
+  public void checkWinner_rockBeatsScissors_true() {
+    RPSGame rpsGame = new RPSGame();
+    assertEquals(true, rpsGame.checkWinner("Rock", "Scissors"));
+  }
+
+  @Test
+  public void checkWinner_ScissorsBeatsRocks_false() {
+    RPSGame rpsGame = new RPSGame();
+    assertEquals(false, rpsGame.checkWinner("Scissors", "Rock"));
+  }
+}
+
+ /*rock Scissors true
+Scissors Rock  false
+rock paper false
+Paper Rock true
+scissors paper true
+paper scissors false
+*/
